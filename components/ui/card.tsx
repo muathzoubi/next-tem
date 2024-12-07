d21bd498117b45b2eb9f87fe7988ccf558ebbc0c
muathzoubi <moathaza@gmail.com>
2024-12-06 23:55:37 -0800

    rounded

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border  text-white shadow-sm rounded",
+      "rounded-lg border  text-white shadow-sm ",
       className
     )}
     {...props}
